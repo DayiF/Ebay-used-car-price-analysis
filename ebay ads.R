51808c661e44a24ee6b97846f5649f7eb1b6dca1
DayiF <dayifang4@gmail.com>
2017-02-22 12:16:51 -0500

    Add files via upload

--- ebay ads.R
+++ ebay ads.R
@@ -1,5 +1,6 @@
 library(lubridate)
 library(ggplot2)
+library(gridExtra)
 getwd()
 setwd("C:/Users/df84/Desktop/lets do it/New Projects")
 
@@ -39,6 +40,7 @@ p4 <- ggplot(aes(x="price", y=price), data = mydata) +
   geom_boxplot() +
   ylim(0, quantile(mydata$price, 0.90))
 
+grid.arrange(p1, p2, p3, p4, ncol = 2)
 # From the graph, it is reasonable for us to conclude that price greater than 15000 can be considered as outliers. 
 # Therefore, we can use 0-0.9 range for the clean data.
 mydata <- subset(mydata, price < quantile(mydata$price, 0.90))
@@ -46,6 +48,15 @@ mydata <- subset(mydata, price < quantile(mydata$price, 0.90))
 # powerPS also has outliers, and we need to convert it from factor to numeric data
 mydata$powerPS = as.numeric(levels(mydata$powerPS)[mydata$powerPS])
 summary(mydata$powerPS)
+quantile(mydata$powerPS, 0.1)
+quantile(mydata$powerPS, 0.90)
+quantile(mydata$powerPS, 0.95)
+
+ggplot(aes(x=vehicleType, y=powerPS), data = mydata) + 
+  geom_boxplot() +
+  ylim(quantile(mydata$powerPS, 0.05), quantile(mydata$powerPS, 0.95))
+# select 0.05-0.95 for throwing out outliers
+mydata <- subset(mydata, quantile(mydata$powerPS, 0.05) < powerPS & powerPS < quantile(mydata$powerPS, 0.95))
 
 # need to clean the dataset and convert the date format 
 mydata$dateCrawled <- ymd(mydata$dateCrawled) 
